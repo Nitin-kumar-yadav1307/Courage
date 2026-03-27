12a37a145c84ab396ac056ec96946a49fb24fe62
Nitin Kumar Yadav <nitinyadav8675@gmail.com>
2026-03-27 18:47:30 +0530

    Week 5 Day 28 — Drawing text on canvas

--- renderer.js
+++ renderer.js
@@ -4,4 +4,9 @@ let ctx = canvas.getContext('2d');
 
 ctx.fillStyle = 'red';
 
-ctx.fillRect(50, 50, 200, 100)
+ctx.fillRect(50, 50, 200, 100);
+
+ctx.fillStyle = 'blue';
+ctx.font = ' 24px sans-serif';
+
+ctx.fillText('shree ganesh',100,100)
